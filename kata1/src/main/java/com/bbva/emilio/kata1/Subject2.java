37d02c884c9ee740d90a2bcc9a2df22a337b9dea
Emilio J Alarcon <ejalarcon@gmail.com>
2016-02-01 16:11:27 +0100

    Se sube Subject2

---
+++ kata1/src/main/java/com/bbva/emilio/kata1/Subject2.java
@@ -0,0 +1,15 @@
+package com.bbva.emilio.kata1;
+
+public class Subject2
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
